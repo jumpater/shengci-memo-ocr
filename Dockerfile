c567505de76e9df15d62044f63e229be682c86b8
Junpei Nitami <xintainjianjunpei@gmail.com>
2022-09-10 00:46:44 +0900

    add docker env

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM ubuntu:latest
+
+RUN apt-get update
+RUN apt-get install -y expect
+RUN apt-get update
+
+#specify location to config timezone(Asia,Tokyo)
+COPY tesseract-installation.exp /tmp
+RUN expect /tmp/tesseract-installation.exp
+
+RUN apt-get update
+RUN apt-get install -y python3-pip tesseract-ocr tesseract-ocr-chi-sim
+ENV TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata
+ADD ./webapp/requirements.txt /tmp/requirements.txt
+RUN pip3 install --no-cache-dir -q -r /tmp/requirements.txt
+ADD ./webapp /opt/webapp/
+WORKDIR /opt/webapp
+RUN mkdir /var/log/gunicorn
+RUN touch /var/log/gunicorn/gunicorn.log
+RUN useradd admin
+USER admin
+CMD gunicorn --bind 0.0.0.0:$PORT hanzi-ocr:app -w 2 -k uvicorn.workers.UvicornWorker --timeout 50
---
+++ webapp/hanzi-ocr.py
@@ -0,0 +1,58 @@
+from PIL import Image, JpegImagePlugin
+import sys, os
+from fastapi import FastAPI, UploadFile, File
+from starlette.requests import Request
+import io
+import pytesseract
+import re
+import jieba
+import json
+
+JpegImagePlugin._getmp = lambda x: None
+
+convert_image = {
+    1: lambda img: img,
+    2: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT),  # 左右反転
+    3: lambda img: img.transpose(Image.ROTATE_180),  # 180度回転
+    4: lambda img: img.transpose(Image.FLIP_TOP_BOTTOM),  # 上下反転
+    5: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(
+        Image.ROTATE_90),  # 左右反転＆反時計回りに90度回転
+    6: lambda img: img.transpose(Image.ROTATE_270),  # 反時計回りに270度回転
+    7: lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(
+        Image.ROTATE_270),  # 左右反転＆反時計回りに270度回転
+    8: lambda img: img.transpose(Image.ROTATE_90),  # 反時計回りに90度回転
+}
+
+
+def read_img(img):
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+    text = pytesseract.image_to_string(img, lang="chi_sim")
+    return (text)
+
+
+app = FastAPI()
+
+
+@app.post("/hanzi-ocr/")
+async def prediction(request: Request, file: bytes = File(...)):
+    if request.method == "POST":
+        image_stream = io.BytesIO(file)
+        image_stream.seek(0)
+        img = Image.open(image_stream, formats=["JPEG", "PNG"])
+        exif = img._getexif()
+        orientation = exif.get(0x112, 1)
+        img = convert_image[orientation](img)
+        print(f"img-format:{img.format}")
+        text = read_img(img)
+        stopWords = [
+            "一",
+            "二",
+            "三",
+            "的",
+        ]
+        wordsList = []
+        for word in jieba.cut_for_search(text):
+            if (re.match("^[\u4E00-\u9FFF]+", word) and word not in stopWords):
+                wordsList.append(word)
+        return json.dumps(wordsList)
+    return "No post request found"
